0993f1af18fbcfafcbc08e0c0621fa077170a963
Jim Meyer <jim@geekdaily.org>
2021-05-08 13:25:40 -0700

    Sync with Tailwind CSS version
    Easier if the gem version == Tailwind CSS version, plus a rev number so we can fix mistakes or make improvements to our wrapper

--- lib/tailwindcss/version.rb
+++ lib/tailwindcss/version.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 module Tailwindcss
-  VERSION = '1.0.3'
+  # Keep gem version in step with version of Tailwind CSS
+  # See  https://github.com/tailwindlabs/tailwindcss/blob/master/CHANGELOG.md
+  # for latest version
+  TAILWIND_VERSION = '2.1.2'
+  GEM_REVISION = ''
+  VERSION = TAILWIND_VERSION + GEM_REVISION
 end
